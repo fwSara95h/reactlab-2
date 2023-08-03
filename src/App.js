332a3be37c342f207a8b1e5d068da92f03c7b780
F W Saarah Hossain <fwsara.h95@gmail.com>
2023-08-03 19:23:15 -0400

    Added header from a different file
    imports practice

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Heading from './Header';
 
 function App() {
   return (
     <div className="App">
+      <Heading />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
