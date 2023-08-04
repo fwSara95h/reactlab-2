30bddfed135943951b6413c37bca304871967275
F W Saarah Hossain <fwsara.h95@gmail.com>
2023-08-04 14:16:12 -0400

    changed Btn, Added small form
    hooks, usesState exercise

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Heading from './Header';
 import Btn from './Btn';
+import RegisterForm from './RegisterForm';
 
 function App() {
   return (
     <div className="App">
       <Heading />
+      <RegisterForm />
       <header className="App-header">
         <Btn />
         <img src={logo} className="App-logo" alt="logo" />
--- src/Btn.js
+++ src/Btn.js
@@ -1,5 +1,9 @@
 function Btn() {
-    const clickHandler = () => alert("CLICKED");
+    const clickHandler = () => {
+        let randomNum = Math.floor(Math.random() * 3) + 1;
+        let userInput = prompt("Type a number from 1 to 4");
+        alert(`Computer guess: ${randomNum}, Your number: ${userInput}`)
+    }
     return (
         <button onClick={clickHandler}>
             Click Me
